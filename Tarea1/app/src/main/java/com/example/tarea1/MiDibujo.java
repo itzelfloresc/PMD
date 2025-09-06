9fef0ea55e0079bef5170e78c6cbf8acfba85621
itzelfloresc <itzelflores@ciencias.unam.mx>
2025-09-05 22:08:07 -0600

    Tarea 1: dibujo con carácteres

---
+++ Tarea1/app/src/main/java/com/example/tarea1/MiDibujo.java
@@ -0,0 +1,12 @@
+package com.example.tarea1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MiDibujo extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
